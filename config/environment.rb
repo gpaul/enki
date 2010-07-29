7019a4397449994f7ca8655692def2c7459a7be6
Gustav Paul <gustav.paul@gmail.com>
2010-07-29 16:06:01 +0200

    Sqlite dependency added

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,7 @@ Rails::Initializer.run do |config|
   config.gem "coderay",  :version => "~> 0.9.3"
   config.gem "lesstile", :version => "~> 0.3"
   config.gem "will_paginate", :version => "~> 2.3", :source => 'http://gemcutter.org'
+  config.gem "sqlite3-ruby", :lib => "sqlite3"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
